9da169b634401abd4d033081f5d1928d8468062f
ahmadsoran <ahmadsorann@gmail.com>
2022-04-16 16:19:50 +0300

    fix layout

---
+++ src/components/contact/contact-form.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+export default function ContactForm() {
+    return (
+        <motion.section
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: .5 }}
+            id="section--form">
+            <form action="#">
+                <h3>make an appointment</h3>
+                <input type="email" name="" placeholder='email' id="" />
+
+                <input type="number" name="" placeholder='Phone' id="" />
+                <div className="flex align-item-center">
+                    <input style={{ width: 'fit-content' }} type="checkbox" title='agree' name="chk" id="" />
+                    <strong>agree to procees personal data</strong>
+                </div>
+                <button>Confirm</button>
+
+            </form>
+        </motion.section>
+    )
+}
